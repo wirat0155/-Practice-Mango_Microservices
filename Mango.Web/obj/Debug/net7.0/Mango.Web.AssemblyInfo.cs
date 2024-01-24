fb3e2f466c5580c0bcaa7368d8fab654098090bc
Wirat Sakorn <sunneed.2555@gmail.com>
2024-01-24 20:03:47 +0700

    login in web

--- Mango.Web/obj/Debug/net7.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net7.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83682e40c7184fd5bc870946b305c1362a9a39cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635231d4eb599f130794e1504f8912129b651ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
